a2810780d8c03d66b2ec9fe6d2582f36682e30fd
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-12 00:24:34 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * followed by a new line, except q and e
- * Return: Aways 0 (Sucess)
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch = 'a';
+	int n;
 
-	while (ch <= 'z')
+	for (n = 48; n < 58; n++)
 	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-		ch++;
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
