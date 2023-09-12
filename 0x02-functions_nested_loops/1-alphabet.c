fea553555c8e5cb77c0295b6cf19bc24b211a0f7
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-12 22:26:25 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,14 @@
-/*
-* File 1-alphabet.c
-*/
+ #include "main.h"
 
+ /** 
+ * print_alphabet - prints lowercase alphabets
+ */
+ void print_alphabet(void) 
+{   
+     char la;
 
+    for (la ='a' ;la <= 'z' ; la++)
+          _putchar (la);
+
+     _putchar ('\n') ;
+} 
