b78f7059173ea7e57378c998a47646707866f8e1
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-12 22:08:26 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,16 @@
  /*
  * File: 0-putchar.c
- *
  */
 
 #include <stdio.h>
 
+/*
+* main-print putchar followed by a new line
+*/
+
 int main(void)
 
 /*
-* main-print putchar followed by a new line
 * The program returns 0
 */
 
