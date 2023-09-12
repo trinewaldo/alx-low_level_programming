2dcfff211955ce1578c3c3042a3bb0aec3375152
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-12 22:06:29 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-something
+ /*
+ * File: 0-putchar.c
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+
+/*
+* main-print putchar followed by a new line
+* The program returns 0
+*/
+
+{
+printf("_putchar");
+printf("\n");
+return (0);
+}
