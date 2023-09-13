fcfb38a674c9cd9afdedeebc2f8f57c92306ad4c
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:20:58 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,29 @@
-something
+#include <stdio.h>
+
+void print_times_table(int n)
+{
+	if (n >= 0 && n <= 15)
+	{
+		for (int i = 0; i <= 15; i++)
+		{
+			printf("%d x %d = %d\n", n, i, n * i);
+		}
+	}
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
