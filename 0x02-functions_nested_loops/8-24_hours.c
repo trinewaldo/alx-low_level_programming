93357e334269b880f9666e01702f5305a3b6ed8c
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:46:48 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,20 +1,23 @@
-   #include "main.h"
-   /**
-   * jack_bauer - prints every minute of the day 
-   *              from 00:00 to 23:59
-   */
-    void jack_bauer(void) 
-  {  
-     int hour, minute;
+#include <stdio.h>
 
-   for (hour = 0; hour <= 23; hour ++) 
-        {
-           for (minute = 0; minute <= 59; minute++)
- { 
-          _putchar ((hour / 10) + ('0');
-          _putchar ((hour % 10) + ('0');
-          _putchar (':');
-          _putchar ((minute / 10) + ('0');
-          _putchar ((minute % 10) + ('0');
-          _putchar ('\n');
-   } 
+void jack_bauer(void)
+{
+	for (int hour = 0; hour < 24; hour++)
+	{ /* Hours range from 0 to 23 */
+		for (int minute = 0; minute < 60; minute++)
+		{ /* Minutes range from 0 to 59 */
+			printf("%02d:%02d\n", hour, minute); /* Print the time in HH:MM format */
+		}
+	}
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
