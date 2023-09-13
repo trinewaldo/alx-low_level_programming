64943c0a13b8348479023f8fc40715a276671996
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-13 23:30:49 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,22 @@
-  #include "main.h"
-  /**
-  * _isalpha - checks for alohabetic character 
-  * @c: character to be checked 
-  * 
-  * Return:1 if character is a letter, 0 otherwise
-  */
-  int _isalpha(int c)
-  {   
-           if((c >= 'a' && c <= 'z') ||
-              (c >= 'A' && c <= 'Z'))
-                      Return (1);
-                 else
-                     Return (0);
-   } 
+#include <stdio.h>
+#include <string.h>
+
+/**
+* _isalpha - checks for alohabetic character 
+* @c: character to be checked 
+* 
+* Return:1 if character is a letter, 0 otherwise
+*/
+
+int main() 
+{
+char str[] = "Hello World!";
+for (int i = 0; i < strlen(str); i++) 
+{
+if (isalpha(str[i])) 
+{
+printf("%c is an alphabetical character\n", str[i]);
+}
+}
+return 0;  
+}
