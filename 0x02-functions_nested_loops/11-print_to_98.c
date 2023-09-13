72ce91df5ff71035716d954aab02618db3c1d695
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:41:59 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,42 @@
-   #include <stdio.h>
-   #include "main.h"
-  /** 
-  * print_to_98 - print all numbers from input to 98
-  * @n: the starting number 
-  */
-    void print_to_98(void n)
-{ 
-   if (n >= 98) 
-  {
-         while (n > 98)
-           printf ("%d, ", n--) 
-             printf("%d\n", n);
-    } 
-     else
-  {
-       while (n < 98)
-         printf("%d,", n++);
-    printf("%d\n", n);
-     } 
-} 
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (int i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+    printf("\n");
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);	
+	return (0);
+}
