6ca9cf757a64666b101e744564f3671070b31330
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:45:29 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,32 @@
-   #include "main.h"
-   /**
-   * _abs - Computes the absolute value of an integer
-   * @t: The integer to be computed
-   *
-   * Return the absolute value 
-   */
-     int _abs(int t)
-       {
-          if ( t >= 0)
-              return (t);
-           else
-              return (-t);
-   } 
+#include <stdio.h>
+
+int _abs(int n) 
+{
+	if (n < 0)
+	{	
+		return -n; /* Return the negation of the negative number to make it positive */
+	} else 
+	{
+		return n;  /* If n is already positive or zero, return it as is */
+	}
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
