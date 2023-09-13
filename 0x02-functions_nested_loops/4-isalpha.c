02216c34b34527c1759a4304fe1a70f4adfc14ed
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:43:04 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
-#include <string.h>
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return 1; /* The character is alphabetic */
+	} else
+	{
+		return 0; /* The character is not alphabetic */
+	}
+}
 
 /**
-* _isalpha - checks for alohabetic character 
-* @c: character to be checked 
-* 
-* Return:1 if character is a letter, 0 otherwise
-*/
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
 
-int main() 
-{
-char str[] = "Hello World!";
-while (int i = 0; i < strlen(str); i++) 
-{
-if (isalpha(str[i])) 
-{
-printf("%c is an alphabetical character\n", str[i]);
-}
-}
-return 0;  
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
