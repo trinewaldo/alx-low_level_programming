2acf2d050c320abad6f07535f9d0c683d7990f1c
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:42:44 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,35 @@
-    #include "main.h"
-   /**
-   * _islower-check if a character is lower case
-   * @c: the character 
-   *
-   * Return: 1 if the lower case is found
-   */
-    int _islower(int c)
-{  
-      if( c >= 'a' ; && c <= 'z')
-                    Return (1);
-          else
-                Return (0) ;
- } 
+#include <stdio.h>
+
+/* Prototype: int _islower(int c) */
+/* Returns 1 if c is lowercase */
+/* Returns 0 otherwise */
+int _islower(int c)
+{
+	/* Check if c is in the range of lowercase ASCII values */
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}else
+	{
+		return 0;
+	}
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
