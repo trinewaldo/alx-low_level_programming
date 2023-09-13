de3b829a1ded4a5797568f63560e6cbf2c9e84de
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:45:01 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,51 @@
-  #include "main.h"
-  /**
-  * print_sign - prints the sign of a number 
-  * @n: the number of the sign to be printed 
-  * Return:1 if the number is greater than zero 
-  *        0 if the number is zero 
-  *       -1 if number is less that zero 
-  /* 
-  int print_sign(int n) 
-  {
-      if (n > 0)
-     {  
-       _putchar ('+');
-      Return (1); 
-   } 
-     else if (n == 0)
-  {
-     _putchar ('0') ;
-     Return (0);
-    }
-   else
-     {
-       _putchar ('-') ;
-       Return (-1);
-     }
-  } 
+#include <stdio.h>
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return 1;
+	} else
+		if (n == 0)
+		{
+			printf("0");
+			return 0;
+		} else
+		{
+			printf("-");
+			return -1;
+		}
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(0);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(0xff);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(-1);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
