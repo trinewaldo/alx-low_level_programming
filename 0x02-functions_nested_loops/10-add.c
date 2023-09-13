1e02d5a23b9ede2601d9f3df045a6e7987aad6a6
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 01:16:56 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,22 @@
-    #include "main.h"
-    /**
-    * add - add two numbers 
-    * @a: the first number 
-    * @b: the second number 
-    *
-    * Return: the addition of both 
-    */
-     int add(int a, int b) 
-  {
-    return (a+b);
-} 
+#include <stdio.h>
+
+int add(int a, int b) 
+{
+	int result = a + b;
+	return result;
+}
+
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);	
+	return (0); 
+}
