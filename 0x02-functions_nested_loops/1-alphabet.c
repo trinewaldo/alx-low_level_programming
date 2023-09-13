a1c341c9b63682aee596c88acc15bdd8d21c53c5
trinewaldo <trinewaldo@gmail.com>
2023-09-13 15:09:30 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = a;
+
+while (letter <= z)
+{
+_putchar(letter);
+letter++;
+}
+_putchar(n);
+return (0); }
+
