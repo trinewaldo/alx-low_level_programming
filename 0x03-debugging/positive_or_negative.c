598c4b2043551c0b09c97fec2504190b8a960270
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 02:30:30 +0300

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,1 +1,21 @@
-positive_or_negative.c
+#include "main.h"
+
+/**
+ * positive_or_negative - Determines if an integer is +ve, -ve, or 0.
+ * @i: The integer to check.
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+	printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+	printf("%d is negative\n", i);
+	}
+	else
+	{
+	printf("%d is zero\n", i);
+	}
+}
