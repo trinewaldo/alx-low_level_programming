4f1b88b001f1c41c7b91330f6c50001607ffd09b
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 00:42:00 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,11 @@
-#include "holberton.h"
+#include <stdio.h>
+
 /**
  * print_alphabet - prints the english alphabet from a-z.
  * Return: Nothing.
  */
-void print_alphabet(void)
+int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+	print("abcdefghijklmnopqrstuvwxyz\n");
+	return 0;
 }
