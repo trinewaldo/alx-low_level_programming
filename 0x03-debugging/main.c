8e96b0204ecea77a719cc1ed1a79b898801e8c7b
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 02:30:02 +0300

    Update main.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,1 +1,16 @@
-main.c
+#include "main.h"
+
+/**
+ * main - Tests a function printing if an integer is positive or negative.
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
