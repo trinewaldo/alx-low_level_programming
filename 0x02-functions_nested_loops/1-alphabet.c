e528e871f2a900982f6f686d55a2fd796806308f
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-14 22:32:08 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  * Description: Use the puts function to print the string and add a new line
  * Return: Always 0 (Success)
  */
-int print_alphabet(void)
+int print_alphabet()
 {
 char letter = 'a';
 
