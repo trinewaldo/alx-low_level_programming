903d07743402f5432201e0caab03ec324cb5305e
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-15 04:42:43 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,14 @@
-2-mul.c
+#include "main.h"
+
+/**
+ * mul - input
+ * Description: multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: the answer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
