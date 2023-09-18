7014f16ee8ff0b6640fe8ff0334a51cad0d83513
trinewaldo <trinewaldo@gmail.com>
2023-09-18 15:38:43 -0700

    Updating pointer values

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * updates the value of pointer to 98
+ * Return nothing.
+ */
+void reset_to_98(int *n)
+{
+  int ret;
+
+  ret = 98;
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int n;
+
+  n = 402;
+printf("n=%d\n", n);
+  reset_to_98(&n);
+  printf("n=%d\n", n);
+  return (0);
+}
