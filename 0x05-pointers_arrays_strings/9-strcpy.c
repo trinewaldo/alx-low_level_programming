8cac3117c1f2f9d98c82d2af6a29c8f86ed4c6fd
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-19 05:58:34 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,17 +11,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+int len, i;
 
-	len = 0;
+len = 0;
 
-	while (src[len] != '\0')
-	{
-		len++;
-	}
+while (src[len] != '\0')
+{
+len++;
+}
 
-	for (i = 0; i < len; i++)
-	{
+for (i = 0; i < len; i++)
+{
 dest[i] = src[i];
 }
 dest[i] = '\0';
