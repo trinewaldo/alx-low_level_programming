a178fdafabe95eb55d744b5ae3b0ba8ebc80501a
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-19 03:56:30 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,23 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev- print reversed string, followed by a new line
+ * @s: pointer to the string to point
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int swap;
+
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+putchar(s[i]);
+}
+putchar('/n');
+
+}
