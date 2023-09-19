047d962be524fe34cdcc6befe703d629c1b47d24
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-19 04:09:41 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,21 @@
-#main.h
+#include "main.h"
+
+/**
+ *puts2 - prints string, followed by a new line,
+ * @str: pointer to the string to point
+ * Return: void
+ */
+int void_puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0' )
+{
+if (i % 2 == 0)
+{
+putchar(str[i]);
+} 
+i++;
+}
+putchar('\n');
+}
