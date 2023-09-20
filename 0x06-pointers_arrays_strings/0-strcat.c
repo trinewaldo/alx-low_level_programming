e9535bac2b85857df303aaab353bf184d5b891ca
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-20 20:37:38 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,19 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int dest_leb =0;
-int i =0;
+    int dest_len = 0;
+    int i = 0;
 
-while(dest[edst_len] !='\0')
-{
-dest[dest_len++;
-}
-while(src[i] !='\0')
-{dest[dest_len + i] = src[i];
-i++;
-}
-dest[dest_len +i] ='\0';
+    while (dest[dest_len] != '\0')
+    {
+        dest_len++;
+    }
+    while (src[i] != '\0')
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[dest_len + i] = '\0';
 
-return dest;
+    return dest;
 }
