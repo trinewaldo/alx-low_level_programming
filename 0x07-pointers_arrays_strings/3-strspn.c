8eea6f9107c73578b80d2949e2b7b85a96c73c81
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-25 18:43:36 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,28 @@
 #include "main.h"
-
 /**
- * Function to check the number of bytes in a string
- * @s: the segment checked
- *
- *Return: Number of bytes, which is conistent with those from accept
+ * _strspn - Entry point
+ * @s:  input
+ * @accept: input
+ * Return: Always 0 (success)
  */
-unasigned int _strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
-  unasigned int n = 0;
-  int r;
+	unsigned int n = 0;
+	int r;
 
-  while(*s)
-  {
-    for(r = 0; accept[r]; r++)
-    {
-      if(*s == accept[r])
-      {
-        n++;
-        break;
-      }
-      else if(accept[r+1] ==0)
-        return (n);
-    }
-    s++;
-  }
-  return (n);
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
