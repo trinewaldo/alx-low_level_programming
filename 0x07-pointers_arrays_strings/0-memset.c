03416f764e460dea2f0e192591e96be0236029a7
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-25 17:09:52 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,21 @@
-#include <stdio.h>
 #include "main.h"
 
+/**
+ * _memset - fill a block of memory
+ * @s: starting address of memory
+ * @b: the desired value
+ * @n: member of bytes to be changed
+ *
+ * Return: Always value
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-char *ptr =s;
+    char *ptr = s;
 
-while (n>0)
-{
-*ptr = b;
-ptr++;
-n--;
-}
-return s;
+    while (n > 0) {
+        *ptr = b;
+        ptr++;
+        n--;
+    }
+    return s;
 }
