20c96727cfc61fc35c4126107a2c6d8c36bf957d
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-25 19:24:11 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * print_chessboard - Entry point
  * @a: array
  * Return: Always 0 (Success)
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i;
