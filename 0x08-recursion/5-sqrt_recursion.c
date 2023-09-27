3835246f1d2fec6ae5ab13312063927c3312690f
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 00:00:54 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,32 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
 
-int _sqrt_recursion(int n)
-{
-    if (n < 0)
-    {
+int helper(int n, int start, int end) {
+    if (start > end) {
         return -1;
     }
-    if(sqrt(n)- (int) sqrt(n) != 0)
+    int mid = (start + end) / 2;
+
+    if (mid * mid == n)
     {
-        return -3;
+        return mid;
     }
-    return sqrt(n);
+
+    if (mid * mid > n) {
+        return helper(n, start, mid - 1);
+    }
+
+    return helper(n, mid + 1, end);
+}
+
+int _sqrt_recursion(int n) {
+    if (n < 0) {
+        return -1;
+    }
+
+    if (n == 0 || n == 1) {
+        return n;
+    }
+
+    return helper(n, 0, n);
 }
