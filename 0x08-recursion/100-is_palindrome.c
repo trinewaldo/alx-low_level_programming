8ef82d49a993d48c5271db39284c07777dba2006
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 01:33:13 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,33 +2,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h> 
 
 bool is_alphanumeric(char c)
 {
-return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
 }
-bool is_palindrome_recursive(char *s, int left, int right)
+bool is_palindrome_helper(char *s, int left, int right)
 {
 if (left >= right)
 {
-return true;
+return (true);
 }
 if (!is_alphanumeric(s[left]))
 {
-return is_palindrome_recursive(s, left + 1, right);
+return (is_palindrome_helper(s, left + 1, right));
 }
 if (!is_alphanumeric(s[right]))
 {
-return is_palindrome_recursive(s, left, right - 1);
+return (is_palindrome_helper(s, left, right - 1));
 }
 if (tolower(s[left]) != tolower(s[right]))
 {
-return false;
+return (false);
 }
-return is_palindrome_recursive(s, left + 1, right - 1);
+return (is_palindrome_helper(s, left + 1, right - 1));
 }
 bool is_palindrome(char *s)
 {
 int length = strlen(s);
-return is_palindrome_recursive(s, 0, length - 1);
+return (is_palindrome_helper(s, 0, length - 1));
 }
