da30f5fc88cb95381e334ab58860c275ed38f6fe
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 00:32:11 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,4 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-#include <stdio.h>
-#include <math.h>
 int is_prime_number(int n);
