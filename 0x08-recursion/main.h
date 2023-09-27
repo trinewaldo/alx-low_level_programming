de1773fd29ff3cc1443ead931ceb6851817b1059
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 01:24:09 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+bool is_alphanumeric(char c);
+bool is_palindrome_recursive(char *s, int left, int right);
+bool is_palindrome(char *s);
