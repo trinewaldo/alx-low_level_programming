6aa1b10a44efc418dbd9333fa9d2ce97a3482d40
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 01:13:16 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,38 +2,38 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int is_alphanumeric(char c) {
-    return (c >= a && c <= z) || (c >= A && c <= Z) || (c >= 0 && c <= 9);
+int is_alphanumeric(char c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= 0 && c <= 9));
 }
-
-char to_lowercase(char c) {
-    if (c >= A && c <= Z) {
-        return c + (a - A);
-    }
-    return c;
+char to_lowercase(char c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (c + ('a' - 'A'));
 }
-
-int is_palindrome(char *s) {
-    int left = 0;
-    int right = strlen(s) - 1;
-
-    while (left < right) {
-        while (left < right && !is_alphanumeric(s[left])) {
-            left++;
-        }
-
-        while (left < right && !is_alphanumeric(s[right])) {
-            right--;
-        }
-
-        if (to_lowercase(s[left]) != to_lowercase(s[right])) {
-            return 0; // Not a palindrome
-        }
-
-        left++;
-        right--;
-    }
-
-    return 1;
+return (c);
+}
+int is_palindrome(char *s)
+{
+int left = 0;
+int right = strlen(s) - 1;
+while (left < right)
+{
+while (left < right && !is_alphanumeric(s[left]))
+{
+left++;
+}
+while (left < right && !is_alphanumeric(s[right]))
+{
+right--;
+}
+if (to_lowercase(s[left]) != to_lowercase(s[right]))
+{
+return (0);
+}
+left++;
+right--;
+}
+return (1);
 }
