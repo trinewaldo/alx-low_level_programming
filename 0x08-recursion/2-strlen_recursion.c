cc65eb2dd1c8c4a1920351e4152943a71667d6c5
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-27 13:54:21 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,9 +3,7 @@
 
 /**
 *Function that returns the length of a string
-*
 *int _strlen_recursion; function that checks the length of string
-*
 *(char *s); the string parameter received
 */
 int _strlen_recursion(char *s)
