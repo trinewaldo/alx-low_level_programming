2fb125e41b9da5171aab28737b998f65e39c6d5b
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 01:50:35 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,1 +1,25 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool compare(char *s1, char *s2) 
+{
+if (*s1 == '\0' && *s2 == '\0') 
+{
+return (true);
+}
+if (*s2 == '*') 
+{
+return ((compare(s1, s2 + 1) || compare(s1 + 1, s2)));
+}
+if (*s1 == *s2 || *s2 == '?') 
+{
+return ((compare(s1 + 1, s2 + 1)));
+}
+return (false);
+}
+int wildcmp(char *s1, char *s2) 
+{
+return ((compare(s1, s2) ? 1 : 0));
+}
