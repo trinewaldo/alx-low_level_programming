3be94b4a2ba721aec98c2dc7df2fd74e62219827
trinewaldo <trinewaldo@gmail.com>
2023-09-27 02:26:45 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* void _print_rev_recursion reverses a string
+* char *s: string parameter received
+*/
+void _print_rev_recursion(char *s)
+{
+int i = strlen(s);
+
+for(int m=i-1; m>=0; m--)
+{
+putchar(s[m]);
+}
+printf("\n);
+}
