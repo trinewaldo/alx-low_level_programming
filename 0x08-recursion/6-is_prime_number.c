8b91f90e1489acd4f853a06e4125bafe6618c6d6
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 00:55:58 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,27 +5,27 @@ int is_divisible(int n, int divisor)
 {
 if (divisor == 1)
 {
-return 0;
+return (0);
 }
 if (divisor == 2)
 {
-return (n % 2 == 0) ? 1 : 0;
+return ((n % 2 == 0) ? 1 : 0);
 }
 if (n % divisor == 0)
 {
-return 1;
+return (1);
 }
-return is_divisible(n, divisor - 1);
+return (is_divisible(n, divisor - 1));
 }
 int is_prime_number(int n)
 {
 if (n <= 1)
 {
- return 0;
+return (0);
 }
 if (n == 2)
 {
-return 1;
+return (1);
 }
-return !is_divisible(n, n - 1);
+return (!is_divisible(n, n - 1));
 }
