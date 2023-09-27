cfcddd91cac81a7b3914e1c1bb5ad0c6f1c45d64
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-27 15:28:47 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,14 +3,16 @@
 
 /**
  * Function that returns the value of x raised to power y
+ * Error: Power lower than 0
+ * Base case: any number raise to 0 is 1
  */
 int _pow_recursion(int x, int y)
 {
     if (y < 0) {
-        return -1; // Error: Power lower than 0
+        return -1;
     }
     if (y == 0) {
-        return 1; // Base case: any number raise to 0 is 1
+        return 1;
     }
     return x * _pow_recursion(x, y - 1); // x raised to power y
 }
