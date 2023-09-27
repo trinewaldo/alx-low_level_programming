7b52f204d9a99e5da06d31fe6060691a60fb247b
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-28 00:53:37 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 
+int is_divisible(int n, int divisor)
+{
+if (divisor == 1)
+{
+return 0;
+}
+if (divisor == 2)
+{
+return (n % 2 == 0) ? 1 : 0;
+}
+if (n % divisor == 0)
+{
+return 1;
+}
+return is_divisible(n, divisor - 1);
+}
 int is_prime_number(int n)
 {
 if (n <= 1)
 {
-return 0;
+ return 0;
 }
-if (n <= 3)
+if (n == 2)
 {
 return 1;
 }
-if (n % 2 == 0 || n % 3 == 0)
-{
-return 0;
+return !is_divisible(n, n - 1);
 }
-for (int i = 5; i * i <= n; i += 6)
-{
-if (n % i == 0 || n % (i + 2) == 0)
-{
-return 0;
-}
-}
-return 1;
-}
-
