505b639d64b9612475da72e89fdce7c0104c26b6
Trine Waldo <39766242+trinewaldo@users.noreply.github.com>
2023-09-29 19:38:31 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-gcc -c *.c
-ar rc liball.a *.o
-ranlib liball.a
+C_FILES=$(find . -maxdepth 1 -type f -name "*.c")
+
+for SOURCE_FILE in $C_FILES; do
+FILE_NAME=$(basename -s .c "$SOURCE_FILE")
+gcc -c "$SOURCE_FILE" -o "${FILE_NAME}.o"
+ar rcs liball.a "${FILE_NAME}.o"
+rm "${FILE_NAME}.o"
+done
+echo "Static library liball.a created successfully."
