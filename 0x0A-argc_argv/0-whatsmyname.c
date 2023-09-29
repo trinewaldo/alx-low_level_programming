ad5da4bc19a837ff1ee6f72bbe3e226990793c7c
Trine Waldo <trinewaldo@gmail.com>
2023-09-30 02:37:21 +0300

    change of name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[1]);
+	return (0);
+}
