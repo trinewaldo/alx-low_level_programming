cb8aa8f2e82e6dd8a8ff76623efb2578c92635ca
trinewaldo <trinewaldo@gmail.com>
2023-10-09 12:07:24 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-struct dog {
-  char* name;
-  float age;
-  char* owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
+
+#endif
